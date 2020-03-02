3f56ab3ee5df540d800bb08c16631e537017aa96
Poorghorban <ziba.poorghorban@gmail.com>
2020-03-02 23:19:34 +0330

    add data file

---
+++ data.py
@@ -0,0 +1,29 @@
+
+
+# list of users 
+# dictionary ---> key(national code user) , value (instance of user class)
+ListUsers = {}
+
+
+# list of employees
+# dictionary ---> key(national code employee) , value (instance of Employee class)
+ListEmployees = {}
+
+
+# list of banks 
+# dictionary ---> key(bank name) , value(instance of Bank class)
+ListBanks = {}
+
+# tuple(not change value) of banks name
+ListBanksName = ('Melli' ,'Mellat' ,'Sepah','Industry and Mine','Tosee Taavon','Saderat','Keshavarzi','Maskan','Post Bank')
+
+# tuple(not change value) of binary equivalent banks name
+ListBinaryBanksName = ('0001','0010','0011','0100','0101','0110','0111','1000','1001')
+
+# tuple(not change value) of accounts type
+ListAccountsType = ('Savings' , 'Current','Long Term Investments' , 'Short Term Investments')
+
+# tuple(not change value) of binary equivalent accounts name
+ListBinaryAccountsType = ('00','01','10','11')
+
+
